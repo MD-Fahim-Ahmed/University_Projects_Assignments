924d4851529906db176d4397ca8bf0d1e420f445
MD Fahim Ahmed <88947453+MD-Fahim-Ahmed@users.noreply.github.com>
2023-10-21 00:58:07 +0600

    Addressbook_java_assignment_ooplab
    Assignment by Fahim Shahriar Sir when I was doing OOP lab on my third semester in UIU.

---
+++ Address Book/src/People.java
@@ -0,0 +1,32 @@
+class People {
+    
+    String name;
+    String phone;
+    String email;
+    String address;
+    
+    public People(String name,String phone,String email,String address){
+        this.name=name;
+        this.phone=phone;
+        this.email=email;
+        this.address=address;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+    
+   
+    public void setPhone(String phone){
+        this.phone=phone;
+    }
+   
+    public void setEmail(String email){
+        this.email=email;
+    }
+ 
+    public void setAddress(String address){
+        this.address=address;
+    }
+    
+}
